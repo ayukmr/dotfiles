9c26e2c59b2f25f7fbe4a57d1f635dc541dc1c24
Ayush Kumar <github@ayushk.xyz>
2023-01-17 20:07:50 -0800

    Sort plugins alphabetically for ZSH

--- zsh/bundle.zsh
+++ zsh/bundle.zsh
@@ -25,14 +25,14 @@ if ! zgen saved; then
     # === General ===
     # ===============
 
-    # [syntax-highlighting] fish-like syntax highlighting
-    zgen load zsh-users/zsh-syntax-highlighting
-
     # [autosuggestions] fish-like autosuggestions
     zgen load zsh-users/zsh-autosuggestions
 
     # [history-substring-search] search for substring in history
     zgen load zsh-users/zsh-history-substring-search
+
+    # [syntax-highlighting] fish-like syntax highlighting
+    zgen load zsh-users/zsh-syntax-highlighting
 
     # [vi-mode] better vi keybinds for zsh
     zgen load jeffreytse/zsh-vi-mode
