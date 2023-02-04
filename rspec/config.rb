a24d17ff4d99fc354f655389196155507409dd63
Ayush Kumar <github@ayushk.xyz>
2023-02-04 13:09:36 -0800

    Disable always treating output as TTY for RSpec

--- rspec/config.rb
+++ rspec/config.rb
@@ -6,9 +6,6 @@ RSpec.configure do |c|
   # enable color in output
   c.color = true
 
-  # always treat output as tty
-  c.tty = true
-
   # format output as documentation
   c.formatter = :documentation
 end
