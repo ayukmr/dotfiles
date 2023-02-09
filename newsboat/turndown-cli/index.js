c39d3fd76d84b113addd7bebb8514d0a99f0a151
Ayush Kumar <github@ayushk.xyz>
2023-02-09 07:45:07 -0800

    Display ASCII images in `turndown` for Newsboat

--- newsboat/turndown-cli/index.js
+++ newsboat/turndown-cli/index.js
@@ -3,24 +3,56 @@
 // ====================
 
 const TurndownService = require('turndown');
+const shell = require('shelljs');
 
 // override escaping
 TurndownService.prototype.escape = (str) => str;
 
-function toMarkdown(data) {
-  // turndown settings
-  const settings = {
-    hr: '***',
-    headingStyle:   'atx',
-    codeBlockStyle: 'fenced'
-  };
+// turndown settings
+const settings = {
+  hr: '***',
+  headingStyle:   'atx',
+  codeBlockStyle: 'fenced'
+};
 
-  const service  = new TurndownService(settings);
+// create service
+const service = new TurndownService(settings);
+
+// add rule for images with links
+service.addRule('asciiImages', {
+  filter: (node) => (
+    node.nodeName === 'IMG' &&
+    node.getAttribute('src').match(/^(http|https):/)
+  ),
+
+  replacement: (content, node) => {
+    const nodeAlt = node.getAttribute('alt');
+    const nodeSrc = node.getAttribute('src');
+
+    if (nodeAlt.length === 1) {
+      return nodeAlt;
+    } else {
+      // create ascii art
+      const { code, stdout } = shell.exec(
+        `curl -fsSL --retry 0 '${nodeSrc}' | magick - -auto-orient jpg:- | jp2a --colors --border --width=80 -`,
+        { silent: true }
+      );
+
+      const markdownImage = `![${nodeAlt}](${nodeSrc})`;
+
+      if (code === 0) {
+        return `\n${'```'}\n${stdout}${'```'}\n${markdownImage}\n${content}`;
+      } else {
+        return `\n${markdownImage}\n${content}`;
+      }
+    }
+  }
+});
+
+// convert stdin into markdown
+process.stdin.on('data', (data) => {
   const markdown = service.turndown(data.toString());
 
   // return markdown
   console.log(markdown);
-}
-
-// convert stdin into markdown
-process.stdin.on('data', toMarkdown);
+});
