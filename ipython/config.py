4cf08ff07bd18305d16b65fa5cf83dd31901b5d2
Ayush Kumar <github@ayushk.xyz>
2023-02-13 16:11:23 -0800

    Reorder settings in config.py for IPython

--- ipython/config.py
+++ ipython/config.py
@@ -10,11 +10,11 @@ sys.path.append(expanduser('~/.cfg/ipython'))
 from prompt import Prompt
 
 def configure(c):
-    # ipython logo as banner
-    c.TerminalInteractiveShell.banner1 = 'IP\33[34m[\33[0my\33[34m]\33[0m | IPython v8.3.0\n'
-
     # set colors
     c.InteractiveShell.colors = 'Linux'
+
+    # ipython logo as banner
+    c.TerminalInteractiveShell.banner1 = 'IP\33[34m[\33[0my\33[34m]\33[0m | IPython v8.3.0\n'
 
     # set title string
     c.TerminalInteractiveShell.term_title_format = '{cwd} – ipython'
