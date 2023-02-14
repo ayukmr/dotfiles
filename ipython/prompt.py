6b6ee3239ac95ed27b47792a88ac002bae179bfc
Ayush Kumar <github@ayushk.xyz>
2023-02-13 16:08:57 -0800

    Remove unused arguments in prompt functions for IPython

--- ipython/prompt.py
+++ ipython/prompt.py
@@ -7,13 +7,13 @@ from IPython.terminal.prompts import Prompts, Token
 # custom prompt
 class Prompt(Prompts):
     # default prompt
-    def in_prompt_tokens(self, cli=None):
+    def in_prompt_tokens(self):
         return [(Token.Prompt, '>> '),]
 
     # output prompt
-    def out_prompt_tokens(self, cli=None):
+    def out_prompt_tokens(self):
         return [(Token.Prompt, '=> '),]
 
     # continuation prompt
-    def continuation_prompt_tokens(self, cli=None, width=None):
+    def continuation_prompt_tokens(self):
         return [(Token.Prompt, '** '),]
