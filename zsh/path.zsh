a83dc6fc33d548fe21bbc30d516af47d790f5768
Ayush Kumar <github@ayushk.xyz>
2023-02-16 07:41:49 -0800

    Remove parentheses when appending to path in path.zsh

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -6,25 +6,25 @@
 path=('/usr/local/bin' '/usr/bin' '/bin' '/usr/sbin' '/sbin')
 
 # golang binaries
-path+=("$HOME/go/bin")
+path+="$HOME/go/bin"
 
 # python binaries
-path+=("$HOME/Library/Python/3.8/bin")
+path+="$HOME/Library/Python/3.8/bin"
 
 # rust binaries
-path+=("$HOME/.cargo/bin")
+path+="$HOME/.cargo/bin"
 
 # ruby binaries
-path+=("$GEM_HOME/bin")
+path+="$GEM_HOME/bin"
 
 # yarn binaries
-path+=("$HOME/.yarn/bin")
+path+="$HOME/.yarn/bin"
 
 # config binaries
-path+=("$HOME/.cfg/bin")
+path+="$HOME/.cfg/bin"
 
 # binaries folder
-path+=("$HOME/.local/bin")
+path+="$HOME/.local/bin"
 
 # export path
 export PATH
