b59b04a385fb398c63596e9d488de24a04a75710
Ayush Kumar <github@ayushk.xyz>
2023-08-22 17:26:04 -0700

    Remove `clear` keybind for ZSH

--- zsh/keybinds.zsh
+++ zsh/keybinds.zsh
@@ -2,18 +2,6 @@
 # === ZSH Keybinds ===
 # ====================
 
-# clear screen
-bindkey -M main  ^K _clear
-bindkey -M vicmd ^K _clear
-
-function _clear {
-  zle push-input
-
-  BUFFER='clear'
-  zle accept-line
-}
-zle -N _clear
-
 # use vim keybinds for selecting completions
 zmodload zsh/complist
 bindkey -M menuselect h vi-backward-char
