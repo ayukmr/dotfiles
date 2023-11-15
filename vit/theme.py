8fce9253b3f3e9462127a80badb135a79028a73c
Ayush Kumar <github@ayushk.xyz>
2023-11-15 10:21:25 -0800

    Add configs for Taskwarrior and VIT

---
+++ vit/theme.py
@@ -0,0 +1,18 @@
+# =================
+# === VIT Theme ===
+# =================
+
+theme = [
+    # list header
+    ('list-header-column',           'black,bold', 'dark magenta'),
+    ('list-header-column-separator', 'black,bold', 'dark magenta'),
+
+    # focused item
+    ('reveal focus', 'black,bold', 'dark blue'),
+
+    # top status
+    ('status', 'dark gray', ''),
+
+    # remove striping
+    ('striped-table-row', '', '', '', '', ''),
+]
