4a82088a24fe9db1e9f9cfef5d41cc4cb2ab5151
Ayush Kumar <github@ayushk.xyz>
2024-03-01 15:56:11 -0800

    Use double quotes for Hammerspoon

--- colemak/hammerspoon/colemak.lua
+++ colemak/hammerspoon/colemak.lua
@@ -4,14 +4,14 @@
 
 -- terminal window filter
 terminalFilter =
-    hs.window.filter.new { 'kitty', 'Alacritty' }
+    hs.window.filter.new { "kitty", "Alacritty" }
 
 -- colemak when unfocused
 terminalFilter:subscribe(hs.window.filter.windowUnfocused, function()
-    hs.keycodes.setLayout('Colemak')
+    hs.keycodes.setLayout("Colemak")
 end)
 
 -- qwerty when focused
 terminalFilter:subscribe(hs.window.filter.windowFocused, function()
-    hs.keycodes.setLayout('ABC')
+    hs.keycodes.setLayout("ABC")
 end)
