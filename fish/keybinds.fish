2cb840774e313bca69315eca88caaa08e39dfbe9
Ayush Kumar <github@ayukmr.com>
2024-09-16 14:13:04 -0700

    Format keybinds.fish

--- fish/keybinds.fish
+++ fish/keybinds.fish
@@ -9,8 +9,8 @@ fish_vi_key_bindings
 set fish_vi_force_cursor 1
 
 # mode cursor shapes
-set fish_cursor_default block
-set fish_cursor_insert line
-set fish_cursor_replace underscore
+set fish_cursor_default     block
+set fish_cursor_insert      line
+set fish_cursor_replace     underscore
 set fish_cursor_replace_one underscore
-set fish_cursor_external line
+set fish_cursor_external    line
